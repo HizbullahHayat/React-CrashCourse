5ea744206abeabd44df163054957ea6ead7a3b46
HizbullahHayat101 <84411812+HizbullahHayat@users.noreply.github.com>
2023-08-31 16:01:16 +0500

    Add files via upload

---
+++ 9-Styling with Classes/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+//Styling With Classes
+import Header from './Components/header';
+import Main from './Components/main';
+import Footer from './Components/footer';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Styling With Classes</h1>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
